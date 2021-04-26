0e90648703d3580c0db122b30b53c74135ae0fe8
nmparfait <mbamu.developer@gmail.com>
2021-04-26 14:49:09 -0400

    using module to select easily the elements

---
+++ app.js
@@ -0,0 +1,9 @@
+import get from './getElement.js';
+
+const URL = 'https://randomuser.me/api';
+
+const img = get('.user-img');
+const title = get('.user-title');
+const value = get('.user-value');
+const btn = get('.btn');
+const btns = [...document.querySelectorAll('.icon')];
