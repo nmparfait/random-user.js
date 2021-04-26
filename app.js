4b46ebfdbd3987cf9fed8a83fb7bb0b7ece8861b
nmparfait <mbamu.developer@gmail.com>
2021-04-26 16:33:57 -0400

    display user on the screen using module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import get from './getElement.js';
 import getUser from './fetchUser.js';
+import displayUser from './displayUser.js';
 
 const img = get('.user-img');
 const title = get('.user-title');
@@ -7,10 +8,12 @@ const value = get('.user-value');
 const btn = get('.btn');
 const btns = [...document.querySelectorAll('.icon')];
 
-const showUser = () => {
+const showUser = async () => {
   //get user from api
-  getUser().then((data) => console.log(data));
 
+  //getUser().then((data) => console.log(data));
+  const person = await getUser();
+  displayUser(person);
   //display user
 };
 
