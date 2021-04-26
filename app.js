8150e3eedc5c7426a26e7b96451c71cd6c415984
nmparfait <mbamu.developer@gmail.com>
2021-04-26 15:28:36 -0400

    setup of module of getting user

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import get from './getElement.js';
-
-const URL = 'https://randomuser.me/api';
+import getUser from './fetchUser.js';
 
 const img = get('.user-img');
 const title = get('.user-title');
 const value = get('.user-value');
 const btn = get('.btn');
 const btns = [...document.querySelectorAll('.icon')];
+
+const showUser = () => {
+  //get user from api
+  getUser().then((data) => console.log(data));
+
+  //display user
+};
+
+window.addEventListener('DOMContentLoaded', showUser);
+window.addEventListener('click', showUser);
